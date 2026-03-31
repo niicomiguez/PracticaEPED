a11db8ab451a11ffd176242ce089dcb5ebf5bec2
niicomiguez <niicomiguez10@gmail.com>
2026-03-31 13:27:03 +0200

    avances

--- src/es/uned/lsi/eped/pract2025_2026/IndexTree.java
+++ src/es/uned/lsi/eped/pract2025_2026/IndexTree.java
@@ -8,13 +8,14 @@ public class IndexTree implements IndexIF {
     protected GTreeIF<Node> index;
 
     public IndexTree(){
-        this.index = new GTree<Node>();
+        this.index = new GTree<>();
         Node contenidoRaiz = new NodeRoot();
         this.index.setRoot(contenidoRaiz);
     }
 
     @Override
     public Seq_PSF retrieveIndex(String p) {
+        if(p==null || p.isEmpty()) return new Seq_PSF();
         char[] letras= p.toCharArray();
         // Variable auxiliar para no perder la raíz actual
         GTreeIF<Node> nodoActual = this.index;
@@ -54,6 +55,7 @@ public class IndexTree implements IndexIF {
 
     @Override
     public void insertIndex(String p, String doc_id, int freq) {
+        if(p==null || p.isEmpty()) return;
         char[] letras= p.toCharArray();
         GTreeIF<Node> nodoActual = this.index;
         for (char letra : letras) {
@@ -88,7 +90,6 @@ public class IndexTree implements IndexIF {
 
             nodoActual = nodoActual.getChild(pos);
         }
-        int posInfo = 1;
         boolean infoEncontrado = false;
         IteratorIF<GTreeIF<Node>> itInfo = nodoActual.getChildren().iterator();
 
@@ -98,19 +99,14 @@ public class IndexTree implements IndexIF {
                 infoEncontrado = true;
                 // Si ya existe la palabra, añadimos el doc_id y freq a su lista
                 ((NodeInfo) hijoInfo.getRoot()).getSeqPSR().add(new Pair_S_F(doc_id, freq));
-            } else {
-                posInfo++;
             }
         }
 
         if (!infoEncontrado) {
             Pair_S_F nuevoPar = new Pair_S_F(doc_id, freq);
-
             NodeInfo nuevoNodoInfo = new NodeInfo(nuevoPar);
-
             GTreeIF<Node> ramaInfo = new GTree<>();
             ramaInfo.setRoot(nuevoNodoInfo);
-
             nodoActual.addChild(1, ramaInfo);
         }
 
@@ -118,6 +114,60 @@ public class IndexTree implements IndexIF {
 
     @Override
     public IteratorIF<Pair_W_SeqPSF> prefixIterator(String prefix) {
-        return null;
+        ListIF<Pair_W_SeqPSF> resultados = new List<>();
+
+        if (prefix == null || prefix.isEmpty()) return resultados.iterator();
+
+        GTreeIF<Node> nodoActual = this.index;
+        char[] palabraFragmentada = prefix.toCharArray();
+
+        for (char letra : palabraFragmentada) {
+            IteratorIF<GTreeIF<Node>> it = nodoActual.getChildren().iterator();
+            boolean encontrada = false;
+
+            while (it.hasNext() && !encontrada) {
+                GTreeIF<Node> hijo = it.getNext();
+                Node contenido = hijo.getRoot();
+
+                if (contenido.getNodeType() == Node.NodeType.INNER) {
+                    char letraNodo = ((NodeInner) contenido).getLetter();
+                    if (letraNodo == letra) {
+                        nodoActual = hijo;
+                        encontrada = true;
+                    } else if (letraNodo > letra) {
+                        return resultados.iterator();
+                    }
+                }
+            }
+
+            if (!encontrada) {
+                return resultados.iterator();
+            }
+        }
+
+
+        return resultados.iterator();
+    }
+    private void explorar(GTreeIF<Node> nodo, String palabraAcumulada, ListIF<Pair_W_SeqPSF> resultados) {
+        IteratorIF<GTreeIF<Node>> it = nodo.getChildren().iterator();
+
+        while (it.hasNext()) {
+            GTreeIF<Node> hijo = it.getNext();
+            Node contenido = hijo.getRoot();
+
+            // CASO A: HEMOS ENCONTRADO UNA PALABRA
+            if (contenido.getNodeType() == Node.NodeType.INFO) {
+                NodeInfo info = (NodeInfo) contenido;
+                // Guardamos el par (La palabra que llevamos + su lista de capítulos)
+                resultados.insert(new Pair_W_SeqPSF(info.getSeqPSR()).getWord(),palabraAcumulada);
+            }
+
+            // CASO B: ES UNA LETRA, HAY QUE SEGUIR BAJANDO
+            else if (contenido.getNodeType() == Node.NodeType.INNER) {
+                NodeInner inner = (NodeInner) contenido;
+                // Llamada recursiva: pasamos el hijo y le sumamos su letra a la palabra
+                explorar(hijo, palabraAcumulada + inner.getLetter(), resultados);
+            }
+        }
     }
 }
